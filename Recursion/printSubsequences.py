815914197438d46978b42adebcd30c0fe9641921
TarunGanesh <tarun.21bce8974@vitapstudent.ac.in>
2024-11-02 11:56:21 +0530

    subsequences

--- Recursion/printSubsequences.py
+++ Recursion/printSubsequences.py
@@ -4,10 +4,14 @@ arr=[3,1,2]
 
 def f(ind,sub,arr):
     if ind>=len(arr):
+        if len(sub)==0:
+            print('Empty')
+            return
         for i in sub:
             print(i,end=' ')
         print()
         return
+    # f(ind + 1, sub, arr) # to print in reverse order
     sub.append(arr[ind])
     f(ind+1,sub,arr)
     sub.pop()
