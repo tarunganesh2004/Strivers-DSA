51a5dd1f527ecff24c5585d999ddc7cac686f150
TarunGanesh <tarun.21bce8974@vitapstudent.ac.in>
2025-02-10 20:43:34 +0530

    Update test case for preorder traversal with additional element

--- Trees/TreeTraversals/preorderTraversal.py
+++ Trees/TreeTraversals/preorderTraversal.py
@@ -36,7 +36,7 @@ class TreeNode:
         return [root.data]+self.preorder(root.left)+self.preorder(root.right)
 
 
-arr = [1, 2, 3, 4, 5]
+arr = [1, 2, 3, 4, 5,6]
 root = TreeNode(arr[0])
 root = root.build_tree(arr)
-print(root.preorder(root))  # type: ignore # [4, 2, 5, 1, 3]
+print(root.preorder(root))   # type: ignore
